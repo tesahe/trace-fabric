552625108ad68ba74155edade6617c86c2a310fe
Tesahe Tunson <teeup.personal@gmail.com>
2026-04-07 20:38:05 +0200

    feat: init protobuf schema and rust build pipeline to verify IPC contract

---
+++ scraper-engine/build.rs
@@ -0,0 +1,14 @@
+use std::io::Result;
+
+
+fn main() -> Result<()> {
+    // when cargo build :
+    // look here, invoke compiler, output
+
+    // relative to Cargo.toml
+    prost_build::compile_protos(&["../proto/lead_v1.proto"], &["../proto"])?;
+
+    println!("cargo:rerun-if-changed=../proto/lead_v1.proto");
+
+    Ok(())
+}
--- scraper-engine/src/main.rs
+++ scraper-engine/src/main.rs
@@ -1,3 +1,29 @@
+
+// organized into mod modules for Protobuf structs
+pub mod schema {
+    include!(concat!(env!("OUT_DIR") ,"/tracefabric.rs"));
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Testing TraceFabric Protobuf Ingestion Pipeline");
+
+    // dummy test HTML
+    let dummy_html = String::from("<html><body><h1>Acme Crop</h1><p>Contact us at 555-0100</p></body></html>");
+
+    // Instantiate a RawLead struct
+    let dummy_lead = schema::RawLead {
+        id: String::from("lead_999_xyz"),
+        source_url: String::from("https://example.com/acme-corp"),
+        company_name: String::from("Acme Corp"),
+        raw_html: dummy_html,
+        timestamp: String::from("2026-04-07T12:00:00Z"),
+    };
+
+    // Instantiate a LeadBatch struct
+    let batch = schema::LeadBatch {
+        leads: vec![dummy_lead],
+    };
+
+
+    println!("Successfully instantiated Protobuf Message:\n{:#?}", batch);
 }
